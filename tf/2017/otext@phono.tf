d141a7100857a2c67e696e77407fd99eaae22189
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2017-10-08 16:35:54 +0200

    version analysis

---
+++ tf/2017/otext@phono.tf
@@ -0,0 +1,11 @@
+@config
+@about=Provides phonetic transcriptions to Hebrew Words
+@author=BHSA Data: Constantijn Sikkel; Phono Notebook: Dirk Roorda
+@coreData=BHSA
+@coreVersion=2017
+@fmt:text-phono-full={phono}{phono_trailer}
+@see=https://rawgit.com/ETCBC/text-fabric/master/phono/phonoTf.html
+@source=Phono Notebook applied to BHSA Data
+@writtenBy=Text-Fabric
+@dateWritten=2017-10-08T07:43:38Z
+
