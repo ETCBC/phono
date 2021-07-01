cfb534715c8e8dffc03e4e66303206420081ebe5
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2021-07-01 18:17:19 +0200

    linted and blackened notebooks using jupytext

---
+++ tf/2021/otext@phono.tf
@@ -0,0 +1,11 @@
+@config
+@about=Provides phonetic transcriptions to Hebrew Words
+@author=BHSA Data: Constantijn Sikkel; Phono Notebook: Dirk Roorda
+@coreData=BHSA
+@coreVersion=2021
+@fmt:text-phono-full={phono}{phono_trailer}
+@see=https://github.com/ETCBC/phono
+@source=Phono Notebook applied to BHSA Data
+@writtenBy=Text-Fabric
+@dateWritten=2021-06-28T11:35:43Z
+
